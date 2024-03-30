b8bd6c492f36d8975c12243351702055f7cd973a
福福 <2795241780@qq.com>
2024-03-30 12:08:33 +0800

    第一次

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
+  <div class="todo-list">
+    <div id="app">
+      <AppHeader />  
+      <TodoList />
+    </div>
+  </div>  
+    
+</template>  
+  
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+import { defineComponent } from 'vue'  
+import AppHeader from './components/AppHeader.vue'
+import TodoList from './components/TodoList.vue' 
+  
+export default defineComponent ({  
+  name: 'App',  
+  components: {  
+    AppHeader,
+    TodoList  
+  },
+})  
+</script>  
+  
+<style scoped>  
+.todo-list {  
+    position: relative;  
+    display: flex;  
+    align-items: center;  
+    flex-direction: column;  
+    height: 100vh;  
+    margin: 0 auto;  
+    padding: 20px;  
+    box-sizing: border-box;  
+    color: white;  
+    text-align: center;  
+    overflow: hidden;   
+  }  
+  
+  .todo-list::before {  
+    content: "";  
+    position: fixed;  
+    top: 0;  
+    left: 50%;
+    transform: translateX(-50%);  
+    width: 100vw;  
+    height: 100vh; 
+    background-image: url('@/assets/bj.jpg');  
+    background-size: cover;  
+    background-position: center;  
+    background-repeat: no-repeat;  
+    z-index: -1; 
+    pointer-events: none;  
+  }  
+  
+  
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="semi-transparent-background">  
+      <input type="text" v-model="newTodo" @keyup.enter="addTodo" placeholder="要添加点什么好呢" />  
+      <button @click="addTodo">添加</button>  
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent ({
+    name: 'AppHeader',
+    components: {
+
+    },
+     
+    data() {  
+      return {  
+        newTodo: '',  
+        todos: [],  
+        allChecked: false  
+      };  
+    }, 
+    methods: {  
+      addTodo() {  
+        if (this.newTodo.trim()) {  
+          this.todos.push({ text: this.newTodo, done: false });  
+          this.newTodo = ''  
+        }  
+      }
+        
+    },
+    
+    watch: {  
+      todos: {  
+        deep: true,  
+        handler() {  
+          this.checkAll();  
+        }  
+      }  
+    }
+})   
+</script>
+
+<style>
+.semi-transparent-background {  
+    background-color: rgba(255, 255, 255, 0.652); 
+    border-radius: 5px;  
+    padding: 10px;
+    display: flex;  
+    justify-content: space-between;  
+    align-items: center;
+    width: 500px;
+    margin-top: 150px;
+    margin-bottom: 20px;
+  }
+
+input[type="text"] {  
+    width: 350px;  
+    height: 25px; 
+    top: 45%; 
+    margin-left: 10px; 
+    padding-left: 10px;
+    color: #000000; 
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 15px; 
+    background-color: rgba(255, 255, 255, 0.701); 
+  }
+
+button {  
+    padding: 10px 20px;
+    margin-right: 10px;
+    color: inherit;  
+    background-color: rgba(238, 46, 46, 0.719);  
+    border: none; 
+    border-radius: 5px;
+    cursor: pointer;   
+}
+
+.todo-list-items {  
+    height: 300px;
+    overflow-y: auto;  
+    padding: 0;  
+    margin: 0;  
+    list-style-type: none;  
+  }
+</style>
